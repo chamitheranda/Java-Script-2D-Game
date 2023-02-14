6b0171472ae1523ed8ae03555d194cefe0b3bbff
chamith <erandachamith322@gmail.com>
2023-02-14 22:27:19 +0530

    Set up sounds

---
+++ script.js
@@ -0,0 +1,17 @@
+// set up sounds
+rs = new Audio("sound/run.mp3");
+rs.loop=true;
+
+js = new Audio("sound/jump.mp3");
+
+is = new Audio("sound/idle.mp3");
+is.loop=true;
+
+ds = new Audio("sound/dead.mp3");
+
+bs = new Audio("sound/background.mp3");
+bs.loop=true;
+var fid = 0;
+
+wins = new Audio("sound/win.mp3");
+
