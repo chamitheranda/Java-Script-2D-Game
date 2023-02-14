1c79a07d3195eb98c01dbb333c435e70c566a3dc
chamith <erandachamith322@gmail.com>
2023-02-14 22:28:59 +0530

    Crete run() and b() functions to create run and background move animation

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ var fid = 0;
 
 wins = new Audio("sound/win.mp3");
 
+var rw = 0 ;
+var n=1;
+//1. Run animation 
+function run(){
+   
+    if(n==9){
+        n=1;
+    }
+    var rimg = document.getElementById("character");
+    rimg.src="images/Run ("+n+").png";
+    n++;   
+}
+//2. background move
+var t = 0;
+var bw = 0 ;
+function b(){
+    var bimg= document.getElementById("b").style.backgroundPositionX=t+"px";
+    t -= 20;
+}
